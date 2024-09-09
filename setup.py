37fbfa55a3503ace3842184800a0948121db0a6b
mihpy <mihai@teodosiu.eu>
2024-09-09 07:10:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = [x.strip() for x in open("requirements.txt", "r").readlines()]
 setup(
     name='0SINTr',
     version='0.1.0',
-    description='Gathering data on target based on username or email address.',
+    description='Gathering open-source data on target based on username or email address.',
     author='0SINTr',
     packages=find_packages(),
     install_requires=requirements,
