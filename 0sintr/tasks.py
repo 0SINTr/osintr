0530554ce228e9e8ce68c90c7a1aca5a62251dce
mihpy <mihai@teodosiu.eu>
2024-09-09 07:09:20 +0300

    Update tasks.py

--- 0sintr/tasks.py
+++ 0sintr/tasks.py
@@ -1,13 +1,37 @@
 from crewai import Task
-from agents import analyst, report_writer
+from agents import google_analyst, hibp_analyst, osind_analyst, report_writer
 from prompts import *
+from crewai_tools import (
+    DirectorySearchTool,
+    JSONSearchTool,
+    MDXSearchTool
+)
 
-# Define Analysis Task
-analysis_task = Task(
-    description=data_analyst_task_description,
-    expected_output=data_analyst_task_output,
-    agent=analyst,
-    async_execution=False,
+# Define Google Data Analysis Task
+google_analysis_task = Task(
+    description=google_analyst_task_description,
+    expected_output=google_analyst_task_output,
+    tools=[DirectorySearchTool,MDXSearchTool],
+    agent=google_analyst,
+    async_execution=True,
+)
+
+# Define HIBP Data Analysis Task
+hibp_analysis_task = Task(
+    description=hibp_analyst_task_description,
+    expected_output=hibp_analyst_task_output,
+    tools=[DirectorySearchTool,JSONSearchTool],
+    agent=hibp_analyst,
+    async_execution=True,
+)
+
+# Define OSINT.Industries Data Analysis Task
+osind_analysis_task = Task(
+    description=osind_analyst_task_description,
+    expected_output=osind_analyst_task_output,
+    tools=[DirectorySearchTool,JSONSearchTool],
+    agent=osind_analyst,
+    async_execution=True,
 )
 
 # Define Report Writing Task
@@ -15,6 +39,7 @@ report_writing_task = Task(
     description=report_writing_task_description,
     expected_output=report_writing_task_output,
     agent=report_writer,
+    context=[google_analysis_task, hibp_analysis_task, osind_analysis_task]
     output_file='OSINT_REPORT.md',
-async_execution=False
+    async_execution=False,
 )
