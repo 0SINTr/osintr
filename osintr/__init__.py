c3149b5c562154b7ff35365a7f6a30cc1cf28ae1
mihpy <thekingpopo1@gmail.com>
2024-09-16 18:18:56 +0300

    Minor fixes

--- osintr/__init__.py
+++ osintr/__init__.py
@@ -1,2 +1,10 @@
 '''0SINTr Project'''
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+import os
+import sys
+
+# Get the absolute path to the package's directory
+package_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Check if the package's directory is already in sys.path, if not, add it
+if package_dir not in sys.path:
+    sys.path.append(package_dir)
