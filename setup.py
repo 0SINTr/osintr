cd54f660394a3aa879b6ebb7909c59c285c93a78
mihpy <thekingpopo1@gmail.com>
2024-09-17 08:12:57 +0300

    Minor fixes

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+requirements = [x.strip() for x in open("requirements.txt", "r").readlines()]
+
+setup(
+    name='osintr',
+    version='0.4.4',
+    description='Gathering open-source data on target based on user input.',
+    author='0SINTr',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'osintr = osintr.main:main'
+        ]
+    },
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],  
+    python_requires='>=3.10',
+)
