cad738db53e04fa29a89e30088f24c81313803ae
mihpy <thekingpopo1@gmail.com>
2024-10-02 14:28:12 +0300

    Update match_urls.py

--- osintr/modules/match_urls.py
+++ osintr/modules/match_urls.py
@@ -8,7 +8,7 @@ import re
 try:
     nlp = spacy.load("en_core_web_sm")
 except OSError:
-    print(Style.BRIGHT + Fore.YELLOW + "[i] Downloading 'en_core_web_sm' model ... Hold on.\n")
+    print(Style.BRIGHT + Fore.YELLOW + "[i] Downloading 'en_core_web_sm' model ... Hold on.\n" + Style.RESET_ALL)
     from spacy.cli import download
     download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
